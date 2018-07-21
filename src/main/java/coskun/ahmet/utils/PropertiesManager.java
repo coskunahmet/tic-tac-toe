6fb2788dc89befbc7eb17de589310f8a72d88e7b
Ahmet Coskun <cookiegarbage+github.com@gmail.com>
2018-07-22 00:39:50 +0300

    -Controlling if properties are valid

--- src/main/java/coskun/ahmet/utils/PropertiesManager.java
+++ src/main/java/coskun/ahmet/utils/PropertiesManager.java
@@ -1,11 +1,14 @@
 package coskun.ahmet.utils;
 
+import coskun.ahmet.exception.PropertiesNotValidException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class PropertiesManager {
@@ -13,6 +16,8 @@ public class PropertiesManager {
     private final static Logger LOGGER = LoggerFactory.getLogger(PropertiesManager.class);
 
     private final static String GAME_CONFIG_FILE = "config" + File.separator + "config.properties";
+
+    private final static String SIZE_OF_PLAYGROUND = "size.of.playground";
 
     private Properties gameProperties;
 
@@ -51,7 +56,45 @@ public class PropertiesManager {
                 }
             }
         }
+    }
 
+    //To check if there is the same char for different player or computer
+    public boolean isPropertiesValid() throws NumberFormatException{
+
+        if(!isThereSameCharForDifferentPlayer())
+            return false;
+
+        if(!isSizeOfPlayGroundValid())
+            return false;
+
+        return true;
+    }
+
+    public boolean isThereSameCharForDifferentPlayer() {
+        Map<String, String> propertiesMap = new HashMap<String, String>();
+        for(Object k: this.gameProperties.keySet()) {
+            String key = (String)k;
+            if(propertiesMap.get(this.gameProperties.getProperty(key)) != null) {
+                return false;
+            }
+
+            propertiesMap.put(this.gameProperties.getProperty(key), this.gameProperties.getProperty(key));
+        }
+
+        return true;
+    }
+
+    //size of playground should be between 3x3 and 10x10
+    public boolean isSizeOfPlayGroundValid() throws NumberFormatException{
+        String sizeOfPlayground = this.gameProperties.getProperty(SIZE_OF_PLAYGROUND);
+
+        int sizeOfPlayGround = Integer.parseInt(sizeOfPlayground);
+
+        if(sizeOfPlayGround < 3  || sizeOfPlayGround > 10) {
+            return false;
+        }
+
+        return true;
     }
 
     public String getProperty(String key) {
